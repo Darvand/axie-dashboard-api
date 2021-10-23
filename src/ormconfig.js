ce84d79f6dfa1840f65ac004e19daaefecce32a4
Darvand <davinchis95@gmail.com>
2021-10-23 16:48:15 -0500

    [FIX] Add ormconfig.js file to let heroku run migrations

---
+++ src/ormconfig.js
@@ -0,0 +1,18 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  logging: true,
+  migrations: [__dirname + '/../migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: './migrations',
+  },
+  autoLoadEntities: true,
+  keepConnectionAlive: true,
+  ssl: { rejectUnauthorized: false },
+};
